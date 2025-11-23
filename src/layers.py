b3854fa40d9776dab765232edf493773dac7f1d5
nvhoa2005 <hoa049170@gmail.com>
2025-11-23 10:37:00 +0700

    create FeedForwardLayer and MultiheadAttention

--- src/layers.py
+++ src/layers.py
@@ -1,8 +1,78 @@
 from torch import nn
-from constants import d_model, seq_len, device
+from constants import d_model, seq_len, device, num_heads, d_k, d_ff, drop_out_rate
 
 import torch
 import math
+
+class MultiheadAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.inf = 1e9
+
+        # W^Q, W^K, W^V in the paper
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+
+        self.dropout = nn.Dropout(drop_out_rate)
+        self.attn_softmax = nn.Softmax(dim=-1)
+
+        # Final output linear transformation
+        self.w_0 = nn.Linear(d_model, d_model)
+
+    def forward(self, q, k, v, mask=None):
+        input_shape = q.shape
+
+        # Linear calculation +  split into num_heads
+        q = self.w_q(q).view(input_shape[0], -1, num_heads, d_k) # (B, L, num_heads, d_k)
+        k = self.w_k(k).view(input_shape[0], -1, num_heads, d_k) # (B, L, num_heads, d_k)
+        v = self.w_v(v).view(input_shape[0], -1, num_heads, d_k) # (B, L, num_heads, d_k)
+
+        # For convenience, convert all tensors in size (B, num_heads, L, d_k)
+        q = q.transpose(1, 2)
+        k = k.transpose(1, 2)
+        v = v.transpose(1, 2)
+
+        # Conduct self-attention
+        attn_values = self.self_attention(q, k, v, mask=mask) # (B, num_heads, L, d_k)
+        concat_output = attn_values.transpose(1, 2)\
+            .contiguous().view(input_shape[0], -1, d_model) # (B, L, d_model)
+
+        return self.w_0(concat_output)
+
+    def self_attention(self, q, k, v, mask=None):
+        # Calculate attention scores with scaled dot-product attention
+        attn_scores = torch.matmul(q, k.transpose(-2, -1)) # (B, num_heads, L, L)
+        attn_scores = attn_scores / math.sqrt(d_k)
+
+        # If there is a mask, make masked spots -INF
+        if mask is not None:
+            mask = mask.unsqueeze(1) # (B, 1, L) => (B, 1, 1, L) or (B, L, L) => (B, 1, L, L)
+            attn_scores = attn_scores.masked_fill_(mask == 0, -1 * self.inf)
+
+        # Softmax and multiplying K to calculate attention value
+        attn_distribs = self.attn_softmax(attn_scores)
+
+        attn_distribs = self.dropout(attn_distribs)
+        attn_values = torch.matmul(attn_distribs, v) # (B, num_heads, L, d_k)
+
+        return attn_values
+
+
+class FeedFowardLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff, bias=True)
+        self.relu = nn.ReLU()
+        self.linear_2 = nn.Linear(d_ff, d_model, bias=True)
+        self.dropout = nn.Dropout(drop_out_rate)
+
+    def forward(self, x):
+        x = self.relu(self.linear_1(x)) # (B, L, d_ff)
+        x = self.dropout(x)
+        x = self.linear_2(x) # (B, L, d_model)
+
+        return x
 
 class PositionalEncoder(nn.Module):
     def __init__(self):
