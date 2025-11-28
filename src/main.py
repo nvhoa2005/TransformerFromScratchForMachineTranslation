1be46d7008e3cdb46819dadb344ccccc7fb7b217
nvhoa2005 <hoa049170@gmail.com>
2025-11-28 09:50:50 +0700

    create Manager

---
+++ src/main.py
@@ -0,0 +1,21 @@
+class Manager:
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+    
+    def validation(self):
+        pass
+    
+    def inference(self):
+        pass
+    
+    def greedy_search(self):
+        pass
+    
+    def beam_search(self):
+        pass
+    
+    def make_mask(self):
+        pass
